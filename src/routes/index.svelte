92d7e0d5b8980204fa5a6cc64e360234a86f0186
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2022-04-30 18:34:31 +0200

    Add more words, and a counter

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -33,41 +33,48 @@
 
 <p class="mb-4 text-lg" class:hidden={loaded}><em>Loading words…</em></p>
 
-<table class="table-auto divide-y-2 divide-gray-400 text-lg">
-	<thead class="text-xl bg-blue-300/25">
-		<tr>
-			<th class="px-4 py-2 border-r border-gray-400 font-normal">Orig.</th>
-			<th
-				class="px-4 border-r border-gray-400 font-normal text-blue-700 anyfine:hover:underline"
-				><a href="/rmnz/docs/dmg.pdf">DMG</a></th
-			>
-			<th
-				class="px-4 border-r border-gray-400 font-normal text-blue-700 anyfine:hover:underline"
-				><a href="/rmnz/docs/eir.pdf">EIr</a></th
-			>
-			<th
-				class="px-4 border-r border-gray-400 font-normal text-blue-700 anyfine:hover:underline"
-				><a href="/rmnz/docs/ijmes-full.pdf">IJMES</a></th
-			>
-			<th class="px-4 font-normal text-blue-700 anyfine:hover:underline"
-				><a href="/rmnz/docs/loc.pdf">LOC</a></th
-			>
-		</tr>
-	</thead>
+<div class:hidden={!loaded}>
+	<p class="text-lg mb-4">
+		There are <code class="text-pink-600">{words.length}</code> words so far. A search function
+		will be added soon.
+	</p>
 
-	<tbody class="divide-y divide-gray-400">
-		{#each words as [orig, roms]}
+	<table class="table-auto divide-y-2 divide-gray-400 text-lg">
+		<thead class="text-xl bg-blue-300/25">
 			<tr>
-				<td
-					lang="ar"
-					class="px-4 py-2 text-2xl border-r border-gray-400 font-persian text-center"
-					>{orig}</td
+				<th class="px-4 py-2 border-r border-gray-400 font-normal">Orig.</th>
+				<th
+					class="px-4 border-r border-gray-400 font-normal text-blue-700 anyfine:hover:underline"
+					><a href="/rmnz/docs/dmg.pdf">DMG</a></th
 				>
-				<td class="px-4 border-r border-gray-400">{roms.dmg}</td>
-				<td class="px-4 border-r border-gray-400">{roms.eir}</td>
-				<td class="px-4 border-r border-gray-400">{roms.ijmes}</td>
-				<td class="px-4">{roms.loc}</td>
+				<th
+					class="px-4 border-r border-gray-400 font-normal text-blue-700 anyfine:hover:underline"
+					><a href="/rmnz/docs/eir.pdf">EIr</a></th
+				>
+				<th
+					class="px-4 border-r border-gray-400 font-normal text-blue-700 anyfine:hover:underline"
+					><a href="/rmnz/docs/ijmes-full.pdf">IJMES</a></th
+				>
+				<th class="px-4 font-normal text-blue-700 anyfine:hover:underline"
+					><a href="/rmnz/docs/loc.pdf">LOC</a></th
+				>
 			</tr>
-		{/each}
-	</tbody>
-</table>
+		</thead>
+
+		<tbody class="divide-y divide-gray-400">
+			{#each words as [orig, roms]}
+				<tr>
+					<td
+						lang="ar"
+						class="px-4 py-2 text-2xl border-r border-gray-400 font-persian text-center"
+						>{orig}</td
+					>
+					<td class="px-4 border-r border-gray-400">{roms.dmg}</td>
+					<td class="px-4 border-r border-gray-400">{roms.eir}</td>
+					<td class="px-4 border-r border-gray-400">{roms.ijmes}</td>
+					<td class="px-4">{roms.loc}</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+</div>
