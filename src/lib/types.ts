111d131b5106f89cf03e1cc1114a746837d515d1
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2025-01-31 18:24:44 -0500

    Make modules plain TS

---
+++ src/lib/types.ts
@@ -0,0 +1,12 @@
+export interface Romanizations {
+	dmg: string;
+	eir: string;
+	ijmes: string;
+	ijmesD: string;
+	is: string;
+	loc: string;
+}
+
+export interface WordsObject {
+	[orig: string]: Romanizations;
+}
