f02615b69120698dc73d094e13ceb9534fcd31e9
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2025-10-22 13:56:45 -0400

    Update dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,11 @@
-import prettier from "eslint-config-prettier";
 import js from "@eslint/js";
-import { includeIgnoreFile } from "@eslint/compat";
-import svelte from "eslint-plugin-svelte";
-import globals from "globals";
-import { fileURLToPath } from "node:url";
 import ts from "typescript-eslint";
-const gitignorePath = fileURLToPath(new URL("./.gitignore", import.meta.url));
+import svelte from "eslint-plugin-svelte";
+import prettier from "eslint-config-prettier";
+import globals from "globals";
 
-export default ts.config(
-	includeIgnoreFile(gitignorePath),
+/** @type {import('eslint').Linter.Config[]} */
+export default [
 	js.configs.recommended,
 	...ts.configs.recommended,
 	...svelte.configs["flat/recommended"],
@@ -24,11 +21,10 @@ export default ts.config(
 	},
 	{
 		files: ["**/*.svelte"],
-
 		languageOptions: {
-			parserOptions: {
-				parser: ts.parser,
-			},
+			parserOptions: { parser: ts.parser },
 		},
+		rules: { "svelte/no-navigation-without-resolve": "warn" },
 	},
-);
+	{ ignores: ["build/", ".svelte-kit/", "dist/"] },
+];
